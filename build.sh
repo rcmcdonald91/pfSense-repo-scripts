87233283a55e8a53e582a3a90605f1dfa3f51eab
Manojav Sridhar <manojav@manojav.com>
2021-06-08 18:23:42 -0400

    add support for 2.5.2

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 export PATH=$PATH:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/root/bin
 
+poudriere ports -u -p ng_ports_252
 poudriere ports -u -p ng_ports_251
 poudriere ports -u -p ng_ports_devel
 poudriere ports -u -p wg_pkg_devel
@@ -11,5 +12,6 @@ poudriere ports -u -p wg_pkg_devel
 for arch in amd64 armv7 aarch64
 do
     poudriere bulk -j freebsd_12-2_$arch -p ng_ports_251 -O wg_pkg_devel -f /usr/local/etc/poudriere.d/pfSense-minion-packages-list
+    poudriere bulk -j freebsd_12-2_$arch -p ng_ports_252 -O wg_pkg_devel  -f /usr/local/etc/poudriere.d/pfSense-minion-packages-list
     poudriere bulk -j freebsd_12-2_$arch -p ng_ports_devel -O wg_pkg_devel  -f /usr/local/etc/poudriere.d/pfSense-minion-packages-list
 done
---
+++ install/install_repo_252.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+CURL=/usr/local/bin/curl
+ARCH=`uname -m`
+
+if [ $ARCH == "arm" ]
+then
+	ARCH="armv7"
+elif  [ $ARCH == "arm64" ]
+then
+	ARCH="aarch64"
+fi
+
+KEY=https://packages.mced.tech/install/poudriere.cert
+REPO=https://packages.mced.tech/packages/freebsd_12-2_${ARCH}-ng_ports_252
+KEY_LOC=/usr/local/etc/ssl/wireguard.crt
+PATCH_LOC=https://packages.mced.tech/install/25_unofficial_packages_list.patch
+$CURL -s $KEY -o $KEY_LOC
+$CURL -s $PATCH_LOC -o /tmp/pkg.patch
+patch -p0 --ignore-whitespace < /tmp/pkg.patch
+REPO_OUTPUT="FreeBSD: { enabled: no } 
+mced: { url: \"$REPO\", 
+mirror_type: \"http\" ,
+signature_type: \"pubkey\",
+pubkey: \"$KEY_LOC\",
+enabled: yes}"
+echo $REPO_OUTPUT > /usr/local/etc/pkg/repos/00_WireGuard.conf
+pkg update
+
