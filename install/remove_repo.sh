3e86ec3fc3ffa003fb5d46abe192d2602e46ab83
vajonam <152501+vajonam@users.noreply.github.com>
2021-06-08 07:44:09 -0400

    Update remove_repo.sh

--- install/remove_repo.sh
+++ install/remove_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mv /etc/inc/pkg-utils.inc.orig /etc/inc/pkg-utils.inc
-rm /usr/local/etc/pkg/repos/WireGuard.conf
+rm /usr/local/etc/pkg/repos/*WireGuard.conf
 rm /usr/local/etc/ssl/wireguard.crt
 pkg update
 
